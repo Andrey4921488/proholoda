d8469d548453d5a36ebd4cfc9e2860881ae5fca2
PR0gramist <Andreygamreckij@gmail.com>
2025-02-24 11:35:34 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+document.addEventListener("DOMContentLoaded", function () {
+  let elements = document.querySelectorAll(".hidden");
+
+  function checkPosition() {
+    let windowHeight = window.innerHeight;
+    elements.forEach((el) => {
+      let positionFromTop = el.getBoundingClientRect().top;
+
+      if (positionFromTop - windowHeight <= 0) {
+        el.classList.add("show");
+      }
+    });
+  }
+
+  window.addEventListener("scroll", checkPosition);
+  checkPosition();
+});
+<script>
+  document.querySelector(".menu-toggle").addEventListener("click", function(){" "}
+  {document.querySelector(".top-nav").classList.toggle("show")});
+</script>;
